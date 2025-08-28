ac337db9fb07f3971c629af3eaf0ccefe571f80a
ChessPark0613 <qkrrbghd613@gmail.com>
2025-08-28 17:23:49 +0900

    텀프로젝트 end

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
@@ -5,20 +5,24 @@ input = sys.stdin.readline
 T = int(input())
 for _ in range(T):
     N = int(input().strip())
-    graph = [0] + list(map(int, input().split()))
+    nodes = [0] + list(map(int, input().split()))
     child = [0] * (N + 1)
 
     for i in range(1, N + 1):
-        child[graph[i]] += 1
+        child[nodes[i]] += 1
 
     leaves = deque(i for i in range(1, N + 1) if child[i] == 0)
+    print(child)
+    print(leaves)
     cnt = 0
 
     while leaves:
         leaf = leaves.popleft()
+        print(leaf)
         cnt += 1
-        nxt = graph[leaf]
+        nxt = nodes[leaf]
         child[nxt] -= 1
+        print(child)
         if child[nxt] == 0:
             leaves.append(nxt)
 
