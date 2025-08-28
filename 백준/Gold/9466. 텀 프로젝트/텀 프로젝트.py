9d7306ce07bb64dcc34f30264d52a0433a85e89c
ChessPark0613 <qkrrbghd613@gmail.com>
2025-08-28 16:41:56 +0900

    [Gold III] Title: 텀 프로젝트, Time: 820 ms, Memory: 238048 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
@@ -1,48 +1,25 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
 T = int(input())
 for _ in range(T):
-    n = int(input().strip())
-    nodes = [0] + list(map(int, input().split()))
+    N = int(input().strip())
+    graph = [0] + list(map(int, input().split()))
+    child = [0] * (N + 1)
 
-    chk = [0] * (n + 1)
-    res = n
+    for i in range(1, N + 1):
+        child[graph[i]] += 1
 
-    for i in range(1, n + 1):
-        if chk[i]:
-            continue
+    leaves = deque(i for i in range(1, N + 1) if child[i] == 0)
+    cnt = 0
 
-        stack = []
-        pos = {}
-        v = i
+    while leaves:
+        leaf = leaves.popleft()
+        cnt += 1
+        nxt = graph[leaf]
+        child[nxt] -= 1
+        if child[nxt] == 0:
+            leaves.append(nxt)
 
-        while True:
-            if chk[v]:
-                while stack:
-                    u = stack.pop()
-                    chk[u] = 1
-                    pos.pop(u, None)
-                break
-
-            if v in pos:
-                start = pos[v]
-                res -= (len(stack) - start)
-
-                while stack:
-                    u = stack.pop()
-                    chk[u] = 1
-                    pos.pop(u, None)
-                break
-
-            pos[v] = len(stack)
-            stack.append(v)
-
-            nv = nodes[v]
-            if chk[nv]:
-                while stack:
-                    u = stack.pop()
-                    chk[u] = 1
-                    pos.pop(u, None)
-                break
-
-            v = nv
-
-    print(res)
+    print(cnt)
