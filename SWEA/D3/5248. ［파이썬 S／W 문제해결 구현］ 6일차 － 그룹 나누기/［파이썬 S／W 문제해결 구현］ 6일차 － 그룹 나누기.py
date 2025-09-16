ae1f46b7ef1a31418598d2be4eb2615812d74906
ChessPark0613 <qkrrbghd613@gmail.com>
2025-09-16 13:34:09 +0900

    [D3] Title: [파이썬 S/W 문제해결 구현] 6일차 - 그룹 나누기, Time: 70 ms, Memory: 54,016 KB -BaekjoonHub

--- SWEA/D3/5248. ［파이썬 S／W 문제해결 구현］ 6일차 － 그룹 나누기/［파이썬 S／W 문제해결 구현］ 6일차 － 그룹 나누기.py
+++ SWEA/D3/5248. ［파이썬 S／W 문제해결 구현］ 6일차 － 그룹 나누기/［파이썬 S／W 문제해결 구현］ 6일차 － 그룹 나누기.py
@@ -1,31 +1,41 @@
-from collections import deque
+def make_set(n):
+    return [x for x in range(n + 1)]
+
+
+def find_set(x):
+    # x의 대표자를 반환
+    if x == parent[x]:
+        return x
+    return find_set(parent[x])
+
+
+def union(x, y):
+    px = find_set(x)
+    py = find_set(y)
+    # 서로 다른 집합일 때, 한 집합의 대표의 부모를 다른 집합의 대표로 변경
+    if px != py:
+        parent[py] = px
+
+
+parent = []
 
 T = int(input())
-
 for tc in range(1, T + 1):
+    # N은 학생수, M은 신청서 수
     N, M = map(int, input().split())
-    nums = list(map(int, input().split()))
-    adj = [[] for _ in range(N + 1)]
+    # 신청서 M장의 정보, 신청서 한 장당 숫자 2개
+    data = list(map(int, input().split()))
+    # N 명의 학생에 대해서 서로소 집합 만들기
+    parent = make_set(N)
+    # 신청서 읽기
+    for i in range(0, M * 2, 2):
+        # data[i]번 학생과 data[i+1]번 학생 같은조 만들기
+        union(data[i], data[i + 1])
 
-    for i in range(0, 2 * M, 2):
-        a, b = nums[i], nums[i + 1]
-        adj[a].append(b)
-        adj[b].append(a)
+    # 몇 개의 조가 만들어졌는지 확인
+    group = set()  # 그룹 대표자의 개수가 그룹의 개수
+    for i in range(1, N + 1):
+        # 모든 학생이 속한 그룹의 대표자를 set에 넣기
+        group.add(find_set(i))
 
-    visited = [False] * (N + 1)
-    ans = 0
-
-    for s in range(1, N + 1):
-        if visited[s]:
-            continue
-        ans += 1
-        q = deque([s])
-        visited[s] = True
-        while q:
-            u = q.popleft()
-            for v in adj[u]:
-                if not visited[v]:
-                    visited[v] = True
-                    q.append(v)
-
-    print(f"#{tc} {ans}")
+    print(f'#{tc} {len(group)}')
