20649c174c4b46c662dbb379004a2fef2e913748
ChessPark0613 <qkrrbghd613@gmail.com>
2025-10-15 15:39:45 +0900

    [Silver III] Title: N과 M (2), Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,17 @@
+N, M = map(int, input().split())
+res = []
+
+
+def cur(x):
+    if len(res) == M:
+        print(*res)
+        return
+
+    for i in range(x, N + 1):
+        if i not in res:
+            res.append(i)
+            cur(i)
+            res.pop()
+
+
+cur(1)
